d2da2ee22b40ce6194bf4d0e0af1158ae2fef3d6
codingkrills <pulkitsinghmail@gmail.com>
2021-09-08 22:08:55 +0530

    initial commit

--- script.js
+++ script.js
@@ -22,10 +22,14 @@ function manageData() {
 
       document.getElementById("msg").innerHTML = "Data Added !";
     } else {
+      let arr = JSON.parse(localStorage.getItem("crud"));
+			arr[id]=name;
+			localStorage.setItem("crud", JSON.stringify(data));
+			document.getElementById('msg').innerHTML='Data updated';
     }
   }
-  selectData();
   document.getElementById("name").value = "";
+  selectData();
 }
 
 function selectData() {
@@ -38,13 +42,35 @@ function selectData() {
     for (let k in arr) {
       html =
         html +
-        `<tr><td>${sno}</td><td>${arr[k]}</td></td><td><a href='javascript:void(0)' onclick="deleteData()">DELETE</a></td></tr>`;
+        `<tr>
+        <td>${sno}</td>
+        <td>${arr[k]}</td>
+        <td><a href="javascript:void(0)" onclick="editData(${k})">Edit</a>&nbsp;</td>
+        <td><a href='javascript:void(0)' onclick="deleteData(${k})">DELETE</a></td>
+        </tr>`;
       sno++;
     }
     document.getElementById("root").innerHTML = html;
   }
 }
 
-function editData() {}
+function editData(eid){
+	id=eid;
+	let arr=JSON.parse(localStorage.getItem('crud'));;
+	document.getElementById('name').value=arr[eid];
+}
 
-function deleteData() {}
+function deleteData(id) {
+  alert("DELETE CALL");
+  alert(id);
+
+  // * Get all The Data
+  let arr = JSON.parse(localStorage.getItem("crud"));
+
+  arr.splice(id, 1);
+
+  // * Set The Updated Data
+
+  localStorage.setItem("crud", JSON.stringify(arr));
+  selectData();
+}
